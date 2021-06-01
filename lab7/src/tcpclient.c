191e68077c85c74dc1ac56a801d98f463e78a267
elenadanilenko97 <helen9757@gmail.com>
2021-06-01 12:34:21 +0000

    LAB7

--- lab7/src/tcpclient.c
+++ lab7/src/tcpclient.c
@@ -7,19 +7,85 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define BUFSIZE 100
+#include <stdbool.h>
+#include <getopt.h>
+//#define BUFSIZE 100
 #define SADDR struct sockaddr
 #define SIZE sizeof(struct sockaddr_in)
 
 int main(int argc, char *argv[]) {
+//------------------start
+char ip_address[255];
+  int buff_size = -1;
+  int server_port = -1;
+  
+  while (true) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"ip_address", required_argument, 0, 0},
+                                      {"buff_size", required_argument, 0, 0},
+                                      {"server_port", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case 0: {
+      switch (option_index) {
+      case 0:
+        // IP-адрес
+        strcpy(ip_address, optarg);
+        break;
+      case 1:
+        // Размер буфера
+        buff_size = atoi(optarg);
+        if (buff_size <= 0)
+        {
+            printf("Size of buffer must be positive number.\n");
+            return 1;
+        }
+        break;
+      case 2:
+        // Порт сервера
+        server_port = atoi(optarg);
+        if (server_port <= 1024)
+        {
+            printf("Port of the server must be > 1024.\n");
+            return 1;
+        }
+        break;
+      default:
+        printf("Index %d is out of options\n", option_index);
+      }
+    } break;
+
+    case '?':
+      printf("Arguments error\n");
+      break;
+    default:
+      fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (!strlen(ip_address) || buff_size == -1 || server_port == -1) {
+    fprintf(stderr, "Using: %s --ip_address 0.0.0.0 --buff_size 1024 --server_port 20001\n",
+            argv[0]);
+    return 1;
+  }
+
+//-------------------end
   int fd;
   int nread;
-  char buf[BUFSIZE];
+  char buf[buff_size];
   struct sockaddr_in servaddr;
-  if (argc < 3) {
+  /*if (argc < 3) {
     printf("Too few arguments \n");
     exit(1);
-  }
+  }*/
 
   if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("socket creating");
@@ -42,7 +108,7 @@ int main(int argc, char *argv[]) {
   }
 
   write(1, "Input message to send\n", 22);
-  while ((nread = read(0, buf, BUFSIZE)) > 0) {
+  while ((nread = read(0, buf, buff_size)) > 0) {
     if (write(fd, buf, nread) < 0) {
       perror("write");
       exit(1);
--- lab7/src/udpclient.c
+++ lab7/src/udpclient.c
@@ -9,14 +9,82 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 20001
-#define BUFSIZE 1024
+#include <stdbool.h>
+#include <getopt.h>
+
+//#define SERV_PORT 20001
+//#define BUFSIZE 1024
 #define SADDR struct sockaddr
 #define SLEN sizeof(struct sockaddr_in)
 
 int main(int argc, char **argv) {
+
+//-------------------------------start
+char ip_address[255];
+  int buff_size = -1;
+  int server_port = -1;
+  
+  while (true) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"ip_address", required_argument, 0, 0},
+                                      {"buff_size", required_argument, 0, 0},
+                                      {"server_port", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case 0: {
+      switch (option_index) {
+      case 0:
+        // IP-адрес
+        strcpy(ip_address, optarg);
+        break;
+      case 1:
+        // Размер буфера
+        buff_size = atoi(optarg);
+        if (buff_size <= 0)
+        {
+            printf("Size of buffer must be positive number.\n");
+            return 1;
+        }
+        break;
+      case 2:
+        // Порт сервера
+        server_port = atoi(optarg);
+        if (server_port <= 1024)
+        {
+            printf("Port of the server must be > 1024.\n");
+            return 1;
+        }
+        break;
+      default:
+        printf("Index %d is out of options\n", option_index);
+      }
+    } break;
+
+    case '?':
+      printf("Arguments error\n");
+      break;
+    default:
+      fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (!strlen(ip_address) || buff_size == -1 || server_port == -1) {
+    fprintf(stderr, "Using: %s --ip_address 0.0.0.0 --buff_size 1024 --server_port 20001\n",
+            argv[0]);
+    return 1;
+  }
+
+//-------------------------------end    
   int sockfd, n;
-  char sendline[BUFSIZE], recvline[BUFSIZE + 1];
+  char sendline[buff_size], recvline[buff_size + 1];
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
@@ -27,7 +95,7 @@ int main(int argc, char **argv) {
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(SERV_PORT);
+  servaddr.sin_port = htons(server_port);
 
   if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 0) {
     perror("inet_pton problem");
@@ -40,13 +108,13 @@ int main(int argc, char **argv) {
 
   write(1, "Enter string\n", 13);
 
-  while ((n = read(0, sendline, BUFSIZE)) > 0) {
+  while ((n = read(0, sendline, buff_size)) > 0) {
     if (sendto(sockfd, sendline, n, 0, (SADDR *)&servaddr, SLEN) == -1) {
       perror("sendto problem");
       exit(1);
     }
 
-    if (recvfrom(sockfd, recvline, BUFSIZE, 0, NULL, NULL) == -1) {
+    if (recvfrom(sockfd, recvline, buff_size, 0, NULL, NULL) == -1) {
       perror("recvfrom problem");
       exit(1);
     }
